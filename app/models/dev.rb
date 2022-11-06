dc7a8cfe7f8b5075cb3eee5bc722d0c15a95cf9c
Mariia <m.goncharenkoo@icloud.com>
2022-11-05 20:11:07 -0400

    completed

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,14 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        self.freebies.map { |freebie| freebie.item_name }.include?(item_name)
+    end
+
+    def give_away(dev, freebie)
+        if freebie.dev_id == self.id
+            freebie.update(dev: dev)
+        end
+    end
 end
